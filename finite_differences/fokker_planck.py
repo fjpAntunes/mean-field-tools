9f507fa66f4a22338f77f0efdf56422bdd0ebaa1
Felipe <felipe.antunes@lemon.energy>
2024-05-10 21:25:38 -0300

    chore: lint

--- finite_differences/fokker_planck.py
+++ finite_differences/fokker_planck.py
@@ -1,45 +1,48 @@
 import numpy as np
 from typing import List
 
-class FokkerPlanckEquation():
-    def __init__(
-            self,
-            space_domain : List[np.array], # meshgrid-like
-            time_domain : np.array, # linspace-like
-            vector_field : List[np.array], 
-            initial_condition : np.array,
-            volatility : np.array,
-            number_of_dimensions : int,
-            tolerance : float
-            ):
 
-            self.space_domain = space_domain
-            self.time_domain = time_domain
-            self.vector_field = vector_field
-            self.initial_condition = initial_condition
-            self.volatility = volatility
-            self.number_of_dimensions = number_of_dimensions
-            self.tolerance = tolerance
-            
-            self._sanity_check()
+class FokkerPlanckEquation:
+    def __init__(
+        self,
+        space_domain: List[np.array],  # meshgrid-like
+        time_domain: np.array,  # linspace-like
+        vector_field: List[np.array],
+        initial_condition: np.array,
+        volatility: np.array,
+        number_of_dimensions: int,
+        tolerance: float,
+    ):
+
+        self.space_domain = space_domain
+        self.time_domain = time_domain
+        self.vector_field = vector_field
+        self.initial_condition = initial_condition
+        self.volatility = volatility
+        self.number_of_dimensions = number_of_dimensions
+        self.tolerance = tolerance
+
+        self._sanity_check()
 
     def _sanity_check(self):
-        self._check_number_of_dimensions_and_space_domain_compatibility()        
+        self._check_number_of_dimensions_and_space_domain_compatibility()
 
     def _check_number_of_dimensions_and_space_domain_compatibility(self):
         if len(self.space_domain) != self.number_of_dimensions:
-             raise Exception('Number of arrays in space domain does not match number of dimensions')        
+            raise Exception(
+                "Number of arrays in space domain does not match number of dimensions"
+            )
 
     def _calculate_divergence_term(self, scalar_field):
         partials = []
-        for dimension,field in enumerate(self.vector_field):
+        for dimension, field in enumerate(self.vector_field):
             dislocation = scalar_field * field
-            partial = np.gradient(dislocation,self.tolerance)
+            partial = np.gradient(dislocation, self.tolerance)
             if self.number_of_dimensions > 1:
-                partial = partial[dimension] 
+                partial = partial[dimension]
             partials.append(partial)
 
         return sum(partials)
 
     def _calculate_second_derivative_term(scalar_field):
-         pass
+        pass
--- finite_differences/test_fokker_planck.py
+++ finite_differences/test_fokker_planck.py
@@ -1,46 +1,54 @@
 import numpy as np
 from finite_differences.fokker_planck import FokkerPlanckEquation
 
-ONE_DIM_DOMAIN = np.linspace(0,1,1_000)
-TIME_DOMAIN = np.linspace(0,1,1_000)
+ONE_DIM_DOMAIN = np.linspace(0, 1, 1_000)
+TIME_DOMAIN = np.linspace(0, 1, 1_000)
 TOLERANCE = TIME_DOMAIN[1] - TIME_DOMAIN[0]
 one_dim_fokker_planck = FokkerPlanckEquation(
-space_domain=[ONE_DIM_DOMAIN],
-time_domain=TIME_DOMAIN,
-vector_field=[ONE_DIM_DOMAIN],
-initial_condition=np.array([1] + [0]*999),
-volatility=1,
-number_of_dimensions=1,
-tolerance=TOLERANCE
+    space_domain=[ONE_DIM_DOMAIN],
+    time_domain=TIME_DOMAIN,
+    vector_field=[ONE_DIM_DOMAIN],
+    initial_condition=np.array([1] + [0] * 999),
+    volatility=1,
+    number_of_dimensions=1,
+    tolerance=TOLERANCE,
 )
 
+
 def test_calculate_divergence_term_one_dim_linear():
-    output = one_dim_fokker_planck._calculate_divergence_term(np.ones_like(ONE_DIM_DOMAIN))
-    np.testing.assert_array_almost_equal(output, np.ones_like(ONE_DIM_DOMAIN),decimal=2)
+    output = one_dim_fokker_planck._calculate_divergence_term(
+        np.ones_like(ONE_DIM_DOMAIN)
+    )
+    np.testing.assert_array_almost_equal(
+        output, np.ones_like(ONE_DIM_DOMAIN), decimal=2
+    )
+
 
 def test_calculate_divergence_term_one_dim_test_quadratic():
     output = one_dim_fokker_planck._calculate_divergence_term(ONE_DIM_DOMAIN)
-    np.testing.assert_almost_equal(output, ONE_DIM_DOMAIN * 2,decimal=2)
+    np.testing.assert_almost_equal(output, ONE_DIM_DOMAIN * 2, decimal=2)
 
 
-X_AXIS = np.linspace(0,1,1_000)
-Y_AXIS = np.linspace(0,1,1_000)
-X,Y =  np.meshgrid(X_AXIS,Y_AXIS, indexing='ij')
-TWO_DIM_DOMAIN = [X,Y]
+X_AXIS = np.linspace(0, 1, 1_000)
+Y_AXIS = np.linspace(0, 1, 1_000)
+X, Y = np.meshgrid(X_AXIS, Y_AXIS, indexing="ij")
+TWO_DIM_DOMAIN = [X, Y]
 two_dim_fokker_planck = FokkerPlanckEquation(
-    space_domain= TWO_DIM_DOMAIN,
+    space_domain=TWO_DIM_DOMAIN,
     time_domain=TIME_DOMAIN,
     vector_field=TWO_DIM_DOMAIN,
     initial_condition=np.zeros_like(TWO_DIM_DOMAIN[0]),
-    volatility=np.array([1,1]),
-    number_of_dimensions= 2,
-    tolerance=TOLERANCE
+    volatility=np.array([1, 1]),
+    number_of_dimensions=2,
+    tolerance=TOLERANCE,
 )
 
+
 def test_calculate_divergence_term_two_dim_test():
-    output = two_dim_fokker_planck._calculate_divergence_term(np.ones((1_000,1_000)))
-    np.testing.assert_array_almost_equal(output,2*np.ones((1_000,1_000)), decimal=2)
+    output = two_dim_fokker_planck._calculate_divergence_term(np.ones((1_000, 1_000)))
+    np.testing.assert_array_almost_equal(output, 2 * np.ones((1_000, 1_000)), decimal=2)
+
 
 def test_calculate_divergence_term_two_dim_test_():
-    output = two_dim_fokker_planck._calculate_divergence_term(scalar_field= X*Y)
-    np.testing.assert_array_almost_equal(output, 4*X*Y, decimal=2)
+    output = two_dim_fokker_planck._calculate_divergence_term(scalar_field=X * Y)
+    np.testing.assert_array_almost_equal(output, 4 * X * Y, decimal=2)
