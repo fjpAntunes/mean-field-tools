fix: fixed calculation error on drift integral

mean_field_tools/deep_bsde/forward_backward_sde.py
@@ -1,6 +1,15 @@
 import torch
 from mean_field_tools.deep_bsde.function_approximator import FunctionApproximator
+from typing import Callable
 
+# Maybe create a path class with time and value - (t,X_t) in general
+
+DriftType = Callable[
+    [torch.Tensor], # Shape should be (num_paths, path_length, time_dim+spatial_dims)
+    torch.Tensor #Shape should be (num_paths, path_length)
+]
+
+def zero_drift(x): return x[:,:,0]*0
 
 class Filtration:
     def __init__(
@@ -46,8 +55,7 @@ class BackwardSDE:
         terminal_condition_function,  # Callable over space dimensions
         # exogenous_process,
         filtration: Filtration,
-        drift=lambda x: x[:, :, 0]
-        * 0,  # Callable over tensors of shape (num_paths, path_length, time+spatial_dimension).
+        drift : DriftType = zero_drift, # Callable over tensors of shape (num_paths, path_length, time+spatial_dimension).
     ):
         self.spatial_dimensions = spatial_dimensions
         self.time_domain = time_domain
@@ -69,39 +77,53 @@ class BackwardSDE:
     #    indexes = torch.perm(self.filtration.number_of_paths)[:number_of_samples]
     #    return self.solution_paths[indexes,:,:]
 
-    def set_drift_path(self):
-        self.drift_path = self.drift(self.filtration.brownian_paths).unsqueeze(-1)
-        self.drift_integral = torch.cumsum(self.drift_path * self.dt, dim=1).unsqueeze(
-            -1
-        )
+    def set_drift_path(self) -> tuple[torch.Tensor, torch.Tensor]:
+        """Calculates drift path and backwards drift integral.
+
+        Returns:
+            self.drift_path : path of the drift function over the samples.
+            self.drift_integral : backwards integral of the drift path over the samples.
+        """
+        self.drift_path = self.drift(self.filtration.brownian_paths)
+        self.drift_integral = torch.sum(self.drift_path, dim = 1) - torch.cumsum(self.drift_path, dim = 1).T
+        self.drift_integral = self.drift_integral * self.dt
+        self.drift_integral = self.drift_integral.T.unsqueeze(-1)
         return self.drift_path, self.drift_integral
 
-    def set_terminal_condition(self, terminal_brownian):
+    def set_terminal_condition(self, terminal_brownian : torch.Tensor) -> torch.Tensor:
+        """Calculates terminal condition for the BSDE
+
+        Args:
+            terminal_brownian : values of the exogenous process at terminal time. Shape should be (num_paths, num_of_spatial_dimensions)
+
+        Returns:
+            self.terminal_condition: value of the terminal condition of the BSDE for each of the sample paths.
+        """        
         self.terminal_condition = self.terminal_condition_function(terminal_brownian)
         return self.terminal_condition
 
-    def set_optimization_target(self, terminal_condition, drift_integral):
-        optimization_target = terminal_condition + drift_integral[:, -1, :].squeeze()
-        optimization_target = optimization_target.repeat(
-            repeats=(1, 1, len(self.time_domain))
-        )
-        optimization_target = optimization_target.reshape(
-            (
-                len(self.time_domain),
-                self.filtration.number_of_paths,
-                self.spatial_dimensions,
-            )
-        )
-        optimization_target = torch.swapaxes(optimization_target, 0, 1)
+    def set_optimization_target(self, terminal_condition : torch.Tensor, drift_integral: torch.Tensor) -> torch.Tensor:
+        """Calculates, for each sampled path and time t, the value
+        $$ \\xi + \\int_t^T f_s ds $$
+        which is the optimization target for the elicitability method of conditional expectation calculation.
+
+        Args:
+            terminal_condition : value of the terminal condition of the BSDE for each of the sample paths. Shape: (num_paths, num_spatial_dimensions)
+            drift_integral : backwards integral of the drift path over the samples.
+
+        Returns:
+            optimization_target : optimization target for elicitability method of conditional expectation calculation.
+        """
+        optimization_target = terminal_condition + torch.swapaxes(drift_integral, 0,2)
+        optimization_target = torch.swapaxes(optimization_target, 0,2)
         return optimization_target
 
     def solve(self, approximator_args: dict = None):
         _, drift_integral = self.set_drift_path()
         terminal_brownian = self.filtration.brownian_paths[:, -1, 1]
         terminal_condition = self.set_terminal_condition(terminal_brownian)
-        optimization_target = self.set_optimization_target(
-            terminal_condition, drift_integral
-        )
+        optimization_target = self.set_optimization_target(terminal_condition, drift_integral)
+        #optimization_target is acting weird on script
         self.y_approximator.minimize_over_sample(
             self.filtration.brownian_paths, optimization_target, **approximator_args
         )

mean_field_tools/deep_bsde/script/terminal_quadratic_no_drift.py
@@ -1,14 +1,15 @@
+'''Tests quadratic no drift'''
 from mean_field_tools.deep_bsde.forward_backward_sde import Filtration, BackwardSDE
+from mean_field_tools.deep_bsde.function_approximator import FunctionApproximatorArtist
 import torch
 
-torch.manual_seed(0)
-
-NUMBER_OF_TIMESTEPS = 101
-TIME_DOMAIN = torch.linspace(0, 1, NUMBER_OF_TIMESTEPS)
-NUMBER_OF_PATHS = 100
+TIME_DOMAIN = torch.linspace(0, 1, 101)
+NUMBER_OF_PATHS = 1000
 SPATIAL_DIMENSIONS = 1
 
 TERMINAL_CONDITION = lambda x: x**2
+
+def ANALYTICAL_SOLUTION(x,t,T): return x**2 + (T - t) 
 
 filtration = Filtration(SPATIAL_DIMENSIONS, TIME_DOMAIN, NUMBER_OF_PATHS)
 filtration.generate_paths()
@@ -22,19 +23,22 @@ bsde = BackwardSDE(
 
 bsde.initialize_approximator()
 
-approximate_solution = bsde.solve(
+
+artist = FunctionApproximatorArtist(
+    save_figures=True,
+    analytical_solution=ANALYTICAL_SOLUTION
+)
+
+bsde.solve(
     approximator_args={
         "batch_size": 100,
-        "number_of_iterations": 500,
-        "number_of_epochs": 5,
+        "number_of_iterations": 5000,
+        "number_of_epochs": 50,
         "number_of_plots": 5,
+        "plotter":  artist
     }
 )
 
+import pdb
 
-def test_approximate_solution_shape():
-    assert approximate_solution.shape == (
-        NUMBER_OF_PATHS,
-        NUMBER_OF_TIMESTEPS,
-        SPATIAL_DIMENSIONS,
-    )
+pdb.set_trace()

mean_field_tools/deep_bsde/script/terminal_quadratic_with_drift.py
@@ -1,4 +1,6 @@
+'''Tests quadratic with drift'''
 from mean_field_tools.deep_bsde.forward_backward_sde import Filtration, BackwardSDE
+from mean_field_tools.deep_bsde.function_approximator import FunctionApproximatorArtist
 import torch
 
 TIME_DOMAIN = torch.linspace(0, 1, 101)
@@ -6,6 +8,9 @@ NUMBER_OF_PATHS = 1000
 SPATIAL_DIMENSIONS = 1
 
 TERMINAL_CONDITION = lambda x: x**2
+def DRIFT(x) : return 2*x[:,:,0]
+
+def ANALYTICAL_SOLUTION(x,t,T): return x**2 + (T - t) + (T**2 - t**2)
 
 filtration = Filtration(SPATIAL_DIMENSIONS, TIME_DOMAIN, NUMBER_OF_PATHS)
 filtration.generate_paths()
@@ -14,10 +19,17 @@ bsde = BackwardSDE(
     spatial_dimensions=SPATIAL_DIMENSIONS,
     time_domain=TIME_DOMAIN,
     terminal_condition_function=TERMINAL_CONDITION,
+    drift=DRIFT,
     filtration=filtration,
 )
 
 bsde.initialize_approximator()
+
+
+artist = FunctionApproximatorArtist(
+    save_figures=True,
+    analytical_solution=ANALYTICAL_SOLUTION
+)
 
 bsde.solve(
     approximator_args={
@@ -25,8 +37,7 @@ bsde.solve(
         "number_of_iterations": 5000,
         "number_of_epochs": 50,
         "number_of_plots": 5,
-        "plotting": True,
-        "save_figures": True,
+        "plotter":  artist
     }
 )
 

mean_field_tools/deep_bsde/test/test_backward_sde_with_drift.py
@@ -1,4 +1,5 @@
 from mean_field_tools.deep_bsde.forward_backward_sde import Filtration, BackwardSDE
+from mean_field_tools.deep_bsde.utils import tensors_are_close
 import torch
 
 torch.manual_seed(0)
@@ -31,27 +32,40 @@ optimization_target = bsde.set_optimization_target(
 )
 
 
-def tensors_are_close(a, b, tolerance):
-    return torch.norm(a - b) < tolerance
 
 
-def test_drift_integral():
+def test_drift_integral_at_0():
     assert tensors_are_close(
-        integral[:, -1, 0].squeeze(), torch.Tensor([1, 1, 1]), 3e-2
+        integral[:, 0, 0].squeeze(), torch.Tensor([1, 1, 1]), 3e-2
+    )
+
+def test_drift_integral_at_T():
+    assert tensors_are_close(
+        integral[:, -1, 0].squeeze(), torch.Tensor([0,0,0]), 3e-2
     )
 
 
 def test_set_terminal_condition():
-
     benchmark = torch.Tensor([0.7749, 0.1563, 0.0753])
     assert tensors_are_close(terminal_condition, benchmark, 1e-3)
+
 
 
 def test_set_optimization_target_shape():
     assert optimization_target.shape == (3, 101, 1)
 
 
-def test_set_optimization_target_value():
+def test_set_optimization_target_value_at_T():
     assert tensors_are_close(
-        optimization_target[:, -1, 0], torch.Tensor([1.7849, 1.1663, 1.0853]), 1e-2
+        optimization_target[:, -1, 0], terminal_condition, 1e-2
     )
+
+def test_set_optimization_target_value_at_0():
+    
+    benchmark = terminal_condition + torch.Tensor([1,1,1])
+    assert tensors_are_close(
+        optimization_target[:,0, 0], benchmark, 1e-1
+    )
+
+
+