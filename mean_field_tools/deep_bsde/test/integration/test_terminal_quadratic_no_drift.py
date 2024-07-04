8f1c0cefc469e62265c2dc6cfa5dd7a469404d23
Felipe <felipe.antunes@lemon.energy>
2024-07-04 20:48:45 -0300

    test: tentative integration test structure

---
+++ mean_field_tools/deep_bsde/test/integration/test_terminal_quadratic_no_drift.py
@@ -0,0 +1,45 @@
+"""Tests quadratic no drift"""
+
+from mean_field_tools.deep_bsde.forward_backward_sde import Filtration, BackwardSDE
+from mean_field_tools.deep_bsde.function_approximator import FunctionApproximatorArtist
+import torch
+
+
+def test_terminal_quadratic_no_drift():
+    TIME_DOMAIN = torch.linspace(0, 1, 101)
+    NUMBER_OF_PATHS = 100
+    SPATIAL_DIMENSIONS = 1
+
+    def TERMINAL_CONDITION(filtration: Filtration):
+        B_T = filtration.brownian_process[:, -1, :]
+        return B_T**2
+
+    def ANALYTICAL_SOLUTION(x, t, T):
+        return x**2 + (T - t)
+
+    filtration = Filtration(SPATIAL_DIMENSIONS, TIME_DOMAIN, NUMBER_OF_PATHS, seed=0)
+
+    bsde = BackwardSDE(
+        terminal_condition_function=TERMINAL_CONDITION,
+        filtration=filtration,
+    )
+
+    bsde.initialize_approximator()
+
+    artist = FunctionApproximatorArtist(
+        save_figures=False, analytical_solution=ANALYTICAL_SOLUTION
+    )
+
+    bsde.solve(
+        approximator_args={
+            "batch_size": 100,
+            "number_of_iterations": 500,
+            "number_of_epochs": 5,
+            "number_of_plots": 5,
+            "plotter": artist,
+        }
+    )
+
+    import pdb
+
+    pdb.set_trace()
